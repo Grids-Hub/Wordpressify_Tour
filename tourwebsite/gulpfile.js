3a3121e87dca8659ef9dae38bf63ce326ff43d3e
Aqsa-Qadir <aqsa.qadir@gridshub.com>
2022-03-10 18:15:20 +0500

    Change Gulp FIle and Package.json

--- tourwebsite/gulpfile.js
+++ tourwebsite/gulpfile.js
@@ -4,24 +4,26 @@ const sass    = require('gulp-sass')(require('sass'));
 const cssnano = require('cssnano');
 
 
-function style() {
+gulp.task('style' , function(){
     let plugins = [
         cssnano(),
     ];
 
-    return gulp.src('./Assets/sass/*scss')
-    .pipe(sass())
+    return gulp.src('./Assets/sass/*scss') //scss file location
+    .pipe(sass()) //sass compiler
     .pipe(postcss(plugins)) //For minified code
-    .pipe(gulp.dest('./Assets/Build/css'))
-};
+    .pipe(gulp.dest('./Assets/Build/css')) // compiled css
+    
+});
 
 
-function watch() {
-    gulp.watch('./Assets/sass/*scss' , style);
-}
+gulp.task('build' , function(){
+    gulp.watch('./Assets/sass/*scss' , gulp.series('style'));
+});
 
-exports.style = style;
-exports.watch = watch;
+gulp.task('default', gulp.series('style' , 'build'));
+
+
 
 
 
